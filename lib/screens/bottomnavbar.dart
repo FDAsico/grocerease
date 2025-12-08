b89946c10958974a420a09a1251017ebb74bfa1c
FDAsico <fasico@gbox.adnu.edu.ph>
2025-12-08 20:37:09 +0800

    Misc

--- lib/screens/bottomnavbar.dart
+++ lib/screens/bottomnavbar.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+// ignore: must_be_immutable
 class BottomMenu extends StatelessWidget {
   final currentPageIndex;
   ValueChanged<int> selectedDestination;
